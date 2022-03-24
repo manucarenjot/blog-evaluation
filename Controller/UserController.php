4ed1d5d67e93a9f3762a5b5f622188d69f172851
manucarenjot <carenjotje@gmail.com>
2022-03-24 13:42:59 +0100

    ajout d'utilisateur finis

--- Controller/UserController.php
+++ Controller/UserController.php
@@ -12,29 +12,43 @@ class UserController extends AbstactController
         if ($this->getPost()) {
             $username = trim(strip_tags($_POST['username']));
             $mail = trim(strip_tags(($_POST['mail'])));
-            $password = trim(strip_tags($_POST['password']));
+            $password = ($_POST['password']);
             $passwordRepeat = trim(strip_tags($_POST['password-repeat']));
             $alert = [];
             if (empty($username)) {
-                $alert[] = 'Un des champs est vide';
+                $alert[] = '<div class="alert-error">Un des champs est vide</div>';
             }
             if (empty($mail)) {
-                $alert[] = 'Un des champs est vide';
+                $alert[] = '<div class="alert-error">Un des champs est vide</div>';
             }
             if (empty($password)) {
-                $alert[] = 'Un des champs est vide';
+                $alert[] = '<div class="alert-error">Un des champs est vide</div>';
             }
             if (empty($passwordRepeat)) {
-                $alert[] = 'Un des champs est vide';
+                $alert[] = '<div class="alert-error">Un des champs est vide</div>';
             }
-            if ($password !== $passwordRepeat) {
-                $alert[] = 'Les mots de passe ne correspondent pas !';
+            if (strlen($username) <= 2 || strlen($username) >= 255) {
+                $alert[] = '<div class="alert-error">Le nom d\'utilisateur doit contenir entre 2 et 255 caractères !</div>';
+            }
+            if (!filter_var($mail, FILTER_VALIDATE_EMAIL)) {
+                $alert[] = '<div class="alert-error">L\'adresse e-mail n\'est pas valide !</div>';
+            }
 
+            if (strlen($password) <= 5 || strlen($password) >= 255) {
+                $alert[] = '<div class="alert-error">Le mot de passe doit contenir entre 5 et 255 caractères !</div>';
+            }
+
+            if ($password !== $passwordRepeat) {
+                $alert[] = '<div class="alert-error">Les mots de passe ne correspondent pas !</div>';
             }
 
             if (count($alert) > 0) {
                 $_SESSION['alert'] = $alert;
                 header('LOCATION: ?c=user&a=register');
+            }
+            else {
+                $passwordHash = password_hash($password, PASSWORD_DEFAULT);
+                UserManager::addUser($username, $mail, $passwordHash);
             }
         }
 
--- Model/UserManager.php
+++ Model/UserManager.php
@@ -1,6 +1,23 @@
 <?php
+
+use App\Connect\Connect;
+
 class UserManager
 
 {
 
+    public static function addUser(string $username, string $mail, string $password) {
+        $insert = Connect::getPDO()->prepare("INSERT INTO fpm03_user (username, mail, password, date) 
+                                            VALUES('{$username}', '{$mail}', '{$password}', NOW())");
+
+       if ($insert->execute()) {
+           $alert = [];
+           $alert[] = '<div class="alert-succes">Inscription réussi !</div>';
+           if (count($alert) > 0) {
+               $_SESSION['alert'] = $alert;
+               header('LOCATION: ?c=home');
+           }
+
+       }
+    }
 }
