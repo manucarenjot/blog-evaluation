ccc7d6212ed2ae9009b19ac6b0f383fef232b7a8
manucarenjot <carenjotje@gmail.com>
2022-04-05 10:03:15 +0200

    validation de formulaire en js

--- View/user/validation.html.php
+++ View/user/validation.html.php
@@ -1,3 +1,16 @@
 <?php
-
+if (!isset($_SESSION['user'])) {
+    $alert[] = '<div class="alert-error">Vous devez être connecté pour accéder à cette page !</div>';
+    if (count($alert) > 0) {
+        $_SESSION['alert'] = $alert;
+    }
+    header('LOCATION: ?c=home');
+}
+if (!isset($_SESSION['codeValidate'])) {
+    $alert[] = '<div class="alert-error">votre lien de validation a expirer !</div>';
+    if (count($alert) > 0) {
+        $_SESSION['alert'] = $alert;
+    }
+    header('LOCATION: ?c=home');
+}
 
